19f650d84c613fe9bd9c6fa7e0ffefe1e6d12d64
JackWilhelm <jgwilhel@ucsc.edu>
2025-04-10 11:24:41 -0700

    Moved experiment 2 visuals

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,12 +1,25 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+/* exported setup, draw */
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+let seed = 239;
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
+const grassColor = "#53502f";
+const skyColor = "#fdc327";
+const stoneColor = "#3a4b56";
+const treeColor = "#214900";
+const roadColor = "#3c3334";
+const roadLineColor = "#fdc327";
+let stripX = 5;
+let stripY = 50;
+let stripPosY = 0;
+const baseRoadWidth = 3;
+const farRoadWidth = 9;
+const treeSpeedY = 0.5;
+const treeSpeedX = 4000;
+const randomColorChangeAmount = 20;
+const trees = 100;
+let treesYList = [];
+let treesXList = [];
+
 const VALUE1 = 1;
 const VALUE2 = 2;
 
@@ -26,19 +39,11 @@ class MyClass {
     }
 }
 
-function resizeScreen() {
-  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
-  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
-  console.log("Resizing...");
-  resizeCanvas(canvasContainer.width(), canvasContainer.height());
-  // redrawCanvas(); // Redraw everything based on new size
-}
-
-// setup() function is called once when the program starts
 function setup() {
-  // place our canvas, making it fit our container
   canvasContainer = $("#canvas-container");
-  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  let canvas = createCanvas(canvasContainer.width()/2, canvasContainer.height()/2);
+  let button = createButton("reimagine").mousePressed(() => seed++);
+  button.position(canvasContainer.width()/2 + 100, 100);
   canvas.parent("canvas-container");
   // resize canvas is the page is resized
 
@@ -49,31 +54,121 @@ function setup() {
     resizeScreen();
   });
   resizeScreen();
+  
+  for (let i = 0; i < trees; i++) {
+    let z = 0.75;
+    treesYList[i] = lerp(height, height/2, random());
+    if (random() < 0.5) {
+      treesXList[i] = ((width/farRoadWidth * 5) * ((random() + (millis() / treeSpeedX) / z) % 1)) + (width/farRoadWidth * 4.5);
+      treesXList[i] += lerp(0, (1/9) * width, treesYList[i]/height);
+    } else {
+      treesXList[i] = ((width/farRoadWidth * 5) * -((random() + (millis() / treeSpeedX) / z) % 1)) + (width/farRoadWidth * 4.5);
+      treesXList[i] -= lerp(0, (1/9) * width, treesYList[i]/height);
+    }
+  }
 }
 
-// draw() function is called repeatedly, it's the main animation loop
+function resizeScreen() {
+  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
+  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  console.log("Resizing...");
+  resizeCanvas(canvasContainer.width()/2, canvasContainer.height()/2);
+  // redrawCanvas(); // Redraw everything based on new size
+}
+
 function draw() {
-  background(220);    
-  // call a method on the instance
-  myInstance.myMethod();
+  randomSeed(seed);
 
-  // Set up rotation for the rectangle
-  push(); // Save the current drawing context
-  translate(centerHorz, centerVert); // Move the origin to the rectangle's center
-  rotate(frameCount / 100.0); // Rotate by frameCount to animate the rotation
-  fill(234, 31, 81);
+  background(100);
+
   noStroke();
-  rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
-  pop(); // Restore the original drawing context
 
-  // The text is not affected by the translate and rotate
-  fill(255);
-  textStyle(BOLD);
-  textSize(140);
-  text("p5*", centerHorz - 105, centerVert + 40);
-}
+  fill(skyColor);
+  rect(0, 0, width, height / 2);
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  fill(grassColor);
+  rect(0, height / 2, width, height / 2);
+  
+  
+  fill(roadColor);
+  beginShape()
+  vertex(width/baseRoadWidth, height);
+  vertex(width/farRoadWidth * 4, height/2);
+  vertex(width/farRoadWidth * 5, height/2);
+  vertex(width/baseRoadWidth * 2, height);
+  endShape(CLOSE);
+  
+  fill(roadLineColor);
+  rect(width/2 - stripX/2, height/2 - stripY + stripPosY, stripX, stripY)
+  stripPosY += 1.5
+  if (stripY + stripPosY > height) {
+    stripPosY = 0
+  }
+  
+  fill(stoneColor);
+  beginShape();
+  vertex(0, height / 2);
+  const steps = 10;
+  for (let i = 0; i < steps + 1; i++) {
+    let x = (width * i) / steps;
+    let y =
+      height / 2 - (random() * random() * random() * height) / 2 - height / 10;
+    vertex(x, y);
+  }
+  vertex(width, height / 2);
+  endShape(CLOSE);
+  
+  
+  for (let i = 0; i < trees; i++) {
+    let newTreeColor = color(
+      red(treeColor) + random(-randomColorChangeAmount, randomColorChangeAmount), 
+     green(treeColor) + random(-randomColorChangeAmount, randomColorChangeAmount), 
+     blue(treeColor) + random(-randomColorChangeAmount, randomColorChangeAmount));
+      fill(newTreeColor);
+    let z = 0.75;
+    treesYList[i] += 1.5
+    let y = treesYList[i];
+    if (treesXList[i] < width/2) {
+      treesXList[i] -= 1.25;
+      if (treesXList[i] < 0) {
+        treesYList[i] = height/2 + (25 * random())
+        treesXList[i] = ((width/farRoadWidth * 5) * (random()) + (width/farRoadWidth * 4.5));
+        treesXList[i] += lerp(0, (1/9) * width, treesYList[i]/height);
+      }
+      else {
+        random()
+        random()
+      }
+    } else {
+      treesXList[i] += 1.25;
+      if (treesXList[i] > width) {
+        treesYList[i] = height/2 + (25 * random())
+        treesXList[i] = ((width/farRoadWidth * 5) * -(random()) + (width/farRoadWidth * 4.5));
+        treesXList[i] -= lerp(0, (1/9) * width, treesYList[i]/height);
+      }
+      else {
+        random()
+        random()
+      }
+    }
+    if (y > height) {
+      if (random() < 0.5) {
+        treesYList[i] = height/2 + (25 * random())
+        treesXList[i] = ((width/farRoadWidth * 5) * (random()) + (width/farRoadWidth * 4.5));
+        treesXList[i] += lerp(0, (1/9) * width, treesYList[i]/height);
+      } else {
+        treesYList[i] = height/2 + (25 * random())
+        treesXList[i] = ((width/farRoadWidth * 5) * -(random()) + (width/farRoadWidth * 4.5));
+        treesXList[i] -= lerp(0, (1/9) * width, treesYList[i]/height);
+      }
+    } else {
+      random()
+      random()
+      random()
+    }
+    y = treesYList[i]
+    let x = treesXList[i];
+    let s = lerp(10, 100, (y - (height/2))/(height/2));
+    triangle(x, y - s - 20, x - s / 4, y, x + s / 4, y);
+  }
 }
